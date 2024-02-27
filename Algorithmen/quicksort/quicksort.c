7abf96c5c81bf71d9f1d00f579a6ef4cbad0628d
Jonas Lütolf <jonas.luetolf@outlook.com>
2024-02-27 08:36:50 +0100

    add quicksort

---
+++ Algorithmen/quicksort/quicksort.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+
+void swapint(int *a, int *b){
+    int temp = *a;
+    *a=*b;
+    *b=temp;
+}
+
+
+void printarr(int *arr, int len) {
+    printf("[");
+    for (int *i=arr; i<arr+len;i++){
+        printf("%d ,",*i);
+    }
+    printf("]\n");
+}
+
+
+void quicksort(int *arr, int len){
+    if (len>1) {
+        int *pivo = arr;
+        int *i = arr+1;
+        int *j = arr + len-1;
+
+        while (i<=j) {
+            while (*i < *pivo) i++;
+            while (*j > *pivo) j--;
+        
+            if (*i > *j && i<j){         
+            swapint(i, j);
+            i++;
+            j--;
+            }
+        }
+
+        swapint(pivo, j);
+
+        quicksort(arr, j-arr);
+        quicksort(j+1, len - (j-arr)-1);
+    }
+}
+
+int main ()
+{
+    int arr[10] = {4,1,5,8,9,2,3,6,7,0};
+    
+    quicksort(arr, 10);
+    
+    printf("sorted array: ");
+    printarr(arr, 10);   
+    
+    return 0;
+}
